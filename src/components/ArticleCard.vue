f24a441a90cd631e5a3a5dca975981dddc5c81c8
menzhulov_aa <shutkadada@yandex.ru>
2023-08-14 00:16:13 +0300

    Base front view

---
+++ src/components/ArticleCard.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="card">
+      <n-card :title="name">
+        <template #cover>
+          <img :src="photoUrl"> 
+        </template>
+        {{ price }}р
+      </n-card>
+    </div>
+  </template>
+  
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import { NCard } from 'naive-ui';
+
+@Options({
+  components: {
+    NCard
+  },
+  props: {
+    name: String,
+    price: Number,
+    photoUrl: String
+  }
+})
+export default class ArticleCard extends Vue {
+  name!: string
+  price!: number
+  photoUrl!: string
+}
+</script>
+
+<style scoped>
+
+</style>
